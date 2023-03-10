e78bc812dada28ce85c971d4890c2b56ad72ecc5
hansol911 <hansol@micehub.com>
2023-03-10 14:18:06 +0900

    test

--- source/lambda_function.py
+++ source/lambda_function.py
@@ -3,38 +3,14 @@ import json
 import base64
 from requests_toolbelt.multipart import decoder
 import validate
-import upload
+#import upload
 
 def lambda_handler(event, context):
     content_type_header = event['headers']['content-type']
     files = get_multipart_file(event['body'], content_type_header)
     
-    result = validate.validate_file(files[1], files[2])
-    if result['statusCode'] != 200:
-        return create_response(result)
-        
-    # err = upload.upload_file_to_s3(files[1], files[2])
-    # if err != None:
-    #     return create_response({'statusCode': 400, 'message': 'File S3 Upload Failed!'})
-    
     return create_response(result)
     
-def get_multipart_file(files, content_type):
-    decode_files = []
-    try:
-        postdata = base64.b64decode(files)
-        for part in decoder.MultipartDecoder(postdata, content_type).parts:
-            disposition = part.headers[b'Content-Disposition']
-            params = {}
-            for dispPart in str(disposition).split(';'):
-                kv = dispPart.split('=', 2)
-                params[str(kv[0]).strip()] = str(kv[1]).strip('\"\'\t \r\n') if len(kv)>1 else str(kv[0]).strip()
-            type = part.headers.get(b'Content-Type', None)
-            decode_files.append({'content': part.content, "type": type, "params": params})
-    except Exception as err:
-        return 'Invalid File. Unable to decode it. Error : ' + str(err)
-    
-    return decode_files 
     
 def create_response(result):
     return {
