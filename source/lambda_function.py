367a71120eebff8914a0620af126a017275cb3b1
hansol911 <hansol@micehub.com>
2023-03-10 10:44:50 +0900

    deploy.yml 수정

--- source/lambda_function.py
+++ source/lambda_function.py
@@ -29,7 +29,7 @@ def get_multipart_file(files, content_type):
             for dispPart in str(disposition).split(';'):
                 kv = dispPart.split('=', 2)
                 params[str(kv[0]).strip()] = str(kv[1]).strip('\"\'\t \r\n') if len(kv)>1 else str(kv[0]).strip()
-            type = part.headers[b'Content-Type'] if b'Content-Type' in part.headers else None
+            type = part.headers[b'Content-Type'] if str(b'Content-Type') in part.headers else None
             decode_files.append({'content': part.content, "type": type, "params": params})
     except Exception as err:
         return 'Invalid File. Unable to decode it. Error : ' + str(err)
