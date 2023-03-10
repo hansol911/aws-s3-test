ef74053a92d112782382dd0260907f79483f02e3
hansol911 <hansol@micehub.com>
2023-03-10 10:01:54 +0900

    오류 수정

--- source/lambda_function.py
+++ source/lambda_function.py
@@ -28,8 +28,8 @@ def get_multipart_file(files, content_type):
             params = {}
             for dispPart in str(disposition).split(';'):
                 kv = dispPart.split('=', 2)
-                params[str(kv[0]).strip()] = str(kv[1]).strip('\"\'\t \r\n') if len(kv)>1 else str(kv[0]).strip()
-            type = str(part.headers[b'Content-Type']) if b'Content-Type' in part.headers else None
+                params[str(kv[0]).strip()] = str(kv[1]).strip('\"\'\t \r\n') if len(str(kv))>1 else str(kv[0]).strip()
+            type = part.headers[b'Content-Type'] if b'Content-Type' in part.headers else None
             decode_files.append({'content': part.content, "type": type, "params": params})
     except Exception as err:
         return 'Invalid File. Unable to decode it. Error : ' + str(err)
